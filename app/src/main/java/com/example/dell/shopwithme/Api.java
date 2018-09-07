b029708e586a9e03abb8905e52f8e4f8031b02db
RebelsterPi <pro.praneshulleri615@gmail.com>
2018-09-07 20:59:04 +0530

    ADDED API

--- app/src/main/java/com/example/dell/shopwithme/Api.java
+++ app/src/main/java/com/example/dell/shopwithme/Api.java
@@ -1,12 +1,21 @@
 package com.example.dell.shopwithme;
 
+import android.provider.ContactsContract;
+
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
 
 /**
  * Created by DELL on 06-09-2018.
  */
 
 public interface Api {
-    String BASE_URL = "https://bitnami-39xfosdmxa.appspot.com/get-items";
+    String BASE_URL = "https://bitnami-39xfosdmxa.appspot.com/";
+    @GET("get-items")
+    Call<List<HandSet>>getPhone(
+
+    );
 
 }
--- app/src/main/java/com/example/dell/shopwithme/activity1.java
+++ app/src/main/java/com/example/dell/shopwithme/activity1.java
@@ -4,24 +4,57 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class activity1 extends AppCompatActivity {
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
-    private String[] myDataset;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity1);
-        mRecyclerView= findViewById(R.id.rv3);
-        mRecyclerView.setHasFixedSize(true);
-        mLayoutManager=new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mAdapter=new MyAdapter(myDataset);
-        mRecyclerView.setAdapter(mAdapter);
+        Retrofit retrofit=new Retrofit.Builder()
+                .baseUrl(Api.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
+
+        GridView gridview =findViewById(R.id.gridview);
+
+
+
+        Api api= retrofit.create(Api.class);
+        Call<List<HandSet>> call=api.getPhone();
+        call.enqueue(new Callback<List<HandSet>>() {
+            @Override
+            public void onResponse(Call<List<HandSet>> call, Response<List<HandSet>> response) {
+             List<HandSet> phones=response.body();
+             for(HandSet hs:phones){
+                 Log.d("model",hs.getModel());
+
+             }
+            }
+
+            @Override
+            public void onFailure(Call<List<HandSet>> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), "hmm..", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
+/* <android.support.v7.widget.RecyclerView
+        android:layout_width="match_parent"
+        android:id="@+id/rv3"
+        android:layout_height="match_parent"
+        android:layout_alignParentTop="true" /> */
